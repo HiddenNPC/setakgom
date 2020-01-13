b985ed74a38f585b366b334b34dd90ef355bbe99
HiddenNPC <soyo1@naver.com>
2020-01-13 16:48:44 +0900

    홈컨트롤러 변경

--- setakproject/src/main/java/com/spring/setak/HomeController.java
+++ setakproject/src/main/java/com/spring/setak/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -24,6 +25,14 @@ public class HomeController {
 	public String washing() {
 		
 		return "washing";
+	}
+	
+	@RequestMapping(value = "/washmending.st")
+	public ModelAndView washmending() {
+		ModelAndView mav = new ModelAndView();
+		
+		
+		return mav;
 	}
 	
 }
