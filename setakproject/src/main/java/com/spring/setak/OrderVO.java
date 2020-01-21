adbfe640c25dc65da9068d3200ff121a46d8caa7
HiddenNPC <soyo1@naver.com>
2020-01-21 12:33:13 +0900

    맨딩맵퍼 수정 및 오더vo String 수정

--- setakproject/src/main/java/com/spring/setak/OrderVO.java
+++ setakproject/src/main/java/com/spring/setak/OrderVO.java
@@ -1,8 +1,5 @@
 package com.spring.setak;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,14 +8,14 @@ public class OrderVO {
 
 	private int order_num;
 	private String member_id;
-	private Date order_date;
+	private String order_date;
 	private String order_phone;
 	private int order_price;
 	private String order_payment;
 	private String order_delicode;
 	private String order_cancel;
 	private String order_status;
-	private Date order_pickup; 	
+	private String order_pickup; 	
 	private String order_name;
 	private String order_address;
 	private String order_request;	
