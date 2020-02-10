c68380e3a3732a3cb5e71b649ca1df3a9c603d7c
HiddenNPC <soyo1@naver.com>
2020-02-10 15:47:13 +0900

    머지 완료

--- setakproject/src/main/java/com/spring/setak/MendingKeepController.java
+++ setakproject/src/main/java/com/spring/setak/MendingKeepController.java
@@ -1,7 +1,6 @@
 package com.spring.setak;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,8 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.spring.order.KeepCartVO;
