dead42ec163d87b384028ebee7e9bd4801db1558
minchoi <minchoi9509@gmail.com>
2020-02-12 12:30:28 +0900

    검색어 설정 있는거

--- setakproject/src/main/java/com/spring/admin_order/AdminOrderController.java
+++ setakproject/src/main/java/com/spring/admin_order/AdminOrderController.java
@@ -49,6 +49,14 @@ public class AdminOrderController {
 		String start = startDate.replace("-", "/").substring(2, startDate.length());
 		String end = endDate.replace("-", "/").substring(2, endDate.length());
 
+		// 검색어 설정 
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("searchType", searchType);
+		map.put("keyword", keyword);
+		map.put("startDate", start);
+		map.put("endDate", end);
+		map.put("statusArr", statusArr);
+		map.put("orderBy", orderBy);
 		
 		int orderSearchCount = adminOrderService.orderSearchCount(map);
 		ArrayList<OrderVO> orderSearchList = adminOrderService.orderSearch(map);
