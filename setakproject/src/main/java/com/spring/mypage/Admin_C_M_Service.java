1b7211a469dc254c0e210417a650b72fe3cc385b
sonkieung <troops1924@naver.com>
2020-02-13 20:35:39 +0900

    관리자 페이지 쿠폰/적립금 완성!!!!!!!!!

--- setakproject/src/main/java/com/spring/mypage/Admin_C_M_Service.java
+++ setakproject/src/main/java/com/spring/mypage/Admin_C_M_Service.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.spring.member.CouponVO;
+import com.spring.member.MileageVO;
 
 public interface Admin_C_M_Service {
 	//일반 리스트
@@ -23,4 +24,27 @@ public interface Admin_C_M_Service {
 	
 	//쿠폰삭제
 	public int deleteCoupon(int coupon);
+	
+	
+	
+	
+	//마일리지
+		//목록
+	public ArrayList<Object> Admin_MileList();
+			
+			//검색
+	public ArrayList<Object> mileSerach (HashMap<String, Object> map);
+			
+			//마일리지수 
+	public int getMileCount();
+			
+			//마일리지수정
+	public int updateMileage(MileageVO params);
+			
+			//마일리지입력
+	public int insertMileage(MileageVO mvo);
+
+			//마일리지 삭제
+	public int deleteMileage(int mile);
+
 }
--- setakproject/src/main/java/com/spring/mypage/Admin_C_M_ServiceImpl.java
+++ setakproject/src/main/java/com/spring/mypage/Admin_C_M_ServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.spring.mapper.Admin_C_M_Mapper;
 import com.spring.member.CouponVO;
+import com.spring.member.MileageVO;
 
 @Service("admin_C_M_Service")
 public class Admin_C_M_ServiceImpl implements Admin_C_M_Service {
@@ -76,6 +77,69 @@ public class Admin_C_M_ServiceImpl implements Admin_C_M_Service {
 		Admin_C_M_Mapper mapper = sqlSession.getMapper(Admin_C_M_Mapper.class);
 		
 		res = mapper.deleteCoupon(coupon);
+		
+		return res;
+		
+		
+	}
+	
+	@Override
+	public ArrayList<Object> Admin_MileList(){
+		ArrayList<Object> list = null;
+		Admin_C_M_Mapper mapper = sqlSession.getMapper(Admin_C_M_Mapper.class);
+		
+		list = mapper.Admin_MileList();
+		
+		return list;
+	}
+	
+	@Override
+	public ArrayList<Object> mileSerach (HashMap<String, Object> map){
+	
+		ArrayList<Object> milelist = null;
+		Admin_C_M_Mapper mapper = sqlSession.getMapper(Admin_C_M_Mapper.class);
+		
+		milelist = mapper.mileSerach(map);
+		
+		return milelist;
+	}
+	
+	@Override
+	public int getMileCount() {
+		int count = 0;
+		Admin_C_M_Mapper mapper = sqlSession.getMapper(Admin_C_M_Mapper.class);
+		count = mapper.getMileCount();
+		
+		return count;
+	}
+	
+	@Override
+	public int updateMileage(MileageVO params) {
+		int res = 0;
+		Admin_C_M_Mapper mapper = sqlSession.getMapper(Admin_C_M_Mapper.class);
+		
+		res = mapper.updateMileage(params);
+		
+		return res;
+	}
+	
+	@Override
+	public int insertMileage(MileageVO cvo) {
+		int res = 0;
+		
+		Admin_C_M_Mapper mapper = sqlSession.getMapper(Admin_C_M_Mapper.class);
+		
+		res = mapper.insertMileage(cvo);
+		
+		return res;
+	}
+	
+	@Override
+	public int deleteMileage(int mile) {
+		int res = 0;
+		Admin_C_M_Mapper mapper = sqlSession.getMapper(Admin_C_M_Mapper.class);
+		
+		res = mapper.deleteMileage(mile);
 		
 		return res;
 		
